a1d740d2ea57425c53f97819cef1a02d80c4082d
Chan Yi <huichanyi@gmail.com>
2016-09-28 19:59:29 -0400

    Added form validation

--- public/angular-app/hotel-display/hotel-display-controller.js
+++ public/angular-app/hotel-display/hotel-display-controller.js
@@ -1,8 +1,9 @@
 angular.module('meanhotel').controller('HotelController', HotelController);
 
-function HotelController(hotelDataFactory, $routeParams) {
+function HotelController($route, hotelDataFactory, $routeParams) {
   var vm = this;
   var id = $routeParams.id;
+  vm.submitted = false;
   hotelDataFactory.hotelDisplay(id).then(function(response) {
     vm.hotel = response;
 		vm.stars = _getStarsArray(response.stars);
@@ -10,4 +11,24 @@ function HotelController(hotelDataFactory, $routeParams) {
 	function _getStarsArray(stars) {
 		return new Array(stars);
 	}
+
+  vm.addReview = function() {
+    var postData = {
+      name: vm.name,
+      rating: vm.rating,
+      review: vm.review
+    };
+
+    if(vm.reviewForm.$valid) {
+      hotelDataFactory.postReview(id, postData).then(function(response){
+        if (response.status === 200) {
+          $route.reload();
+        }
+      }).catch(function(err) {
+        console.log(err);
+      });
+    } else {
+      vm.isSubmitted = true;
+    }
+  }
 }
